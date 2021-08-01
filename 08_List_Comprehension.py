4bf882935a307be9a6a2930d136c9be86c69d834
probsolv <88177071+probsolv@users.noreply.github.com>
2021-08-01 19:04:07 +1000

    Update 08_List_Comprehension.py

--- 08_List_Comprehension.py
+++ 08_List_Comprehension.py
@@ -4,7 +4,12 @@ y = 1
 z = 2
 n = 3
 
-# your code
+# Alternative 1: your code with list comprehension
+
+l = [[i,j,k] for i in range(x+1) for j in range(y+1) for k in range(z+1) if sum([i,j,k]) != n]
+print(l)
+
+# Alternative 2: your code with loops
 
 resultslist = []
 for i in range(0,x+1):
